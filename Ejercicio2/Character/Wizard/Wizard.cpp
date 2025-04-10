cbe02ffe45480c659fd2b9998dda249bbab56e9d
LuloTchicourel <ltchicourel@udesa.edu.ar>
2025-04-10 08:57:49 -0300

    poli

---
+++ Ejercicio2/Character/Wizard/Wizard.cpp
@@ -0,0 +1,19 @@
+#include "Wizard.hpp"
+
+Wizard::Wizard() {}
+
+void Wizard::attack(Character& enemy, string& weapon_name) {
+    Weapon* weapon = get_weapon_by_name(weapon_name);
+    if (!weapon) {
+        cout << "Weapon not found.\n";
+        return;
+    }
+
+    float damage = weapon->get_dmg();
+    float final_damage = damage / (1 + enemy.get_defence());
+    enemy.receive_damage(final_damage);
+    weapon->add_xp(25); 
+    cout << name << " attacks " << enemy.get_name()
+         << " with " << weapon->get_name() << ", dealing "
+         << final_damage << " damage.\n";
+}
--- Ejercicio2/Character/Wizard/Wizard.hpp
+++ Ejercicio2/Character/Wizard/Wizard.hpp
@@ -11,9 +11,10 @@ private:
 public:
     Wizard();
 
-    void attack(Character& enemy) override;
+    void attack(Character& enemy, string& weapon) override;
     bool cast_spell(Character& enemy);
-    
+    void receive_damage(float dmg) override;
+
     string get_name() override;
     void set_custom_name(string n) override;
     float get_hp() override;
@@ -21,6 +22,7 @@ public:
     string show_weapons() override;
     void add_weapon(unique_ptr<Weapon> weapon) override;
     void remove_weapon(string t) override;
+    Weapon* get_weapon_by_name(string& n) override;
     void display() override;
 
     ~Wizard() override = default;
